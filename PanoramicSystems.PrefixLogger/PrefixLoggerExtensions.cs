f0bf84f58d9e831d81854a840d242c428647308c
Daniel Abbatt <daniel.abbatt@panoramic.systems>
2020-04-16 12:15:24 +0100

    Added PrefixedWith extension method

---
+++ PanoramicSystems.PrefixLogger/PrefixLoggerExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace PanoramicSystems.PrefixLogger
+{
+	public static class PrefixLoggerExtensions
+	{
+		public static ILogger PrefixedWith(this ILogger iLogger, string prefix, string? separator = null)
+			=> new PrefixLogger(iLogger, prefix, separator);
+	}
+}
